cabe4ac5dbc0e804ab54e8c8c8625c8786c01c40
rochellev <rochellevroberts@utexas.edu>
2019-06-21 10:53:58 -0700

    added prototypes for Order class

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,23 @@ Pizza.prototype.printPizza = function(){
     console.log("size = "+ this.size + "\n" + "sauce = "+ this.sauce + "\n" + "toppingArray = "+ this.toppingArray + "\n");
 }
 
-// function Order(){
+// keep track of the pizzas
+function Order(){
+    this.id = 0;
+    this.pizzaArray = []
+    this.completeOrder = false;
+}
 
-// }
+Order.prototype.printOrder = function(){
+    console.log("id = "+ this.id + "\n" + "pizzaArray = "+ this.pizzaArray + "\n" + "completeOrder = "+ this.completeOrder + "\n");
+}
 
+Order.prototype.addOrder = function(newPizza){
+    this.pizzaArray.push(newPizza);
+    this.assignId;
+}
+
+Order.prototype.assignId = function(){
+    this.id++
+    return this.id;
+}
--- js/testScript.js
+++ js/testScript.js
@@ -1,3 +1,11 @@
 var mytops = ["pepperoni", "mushroon"];
 var myPizza = new Pizza(2, 2, mytops)
 myPizza.printPizza();
+
+var myOrder = new Order();
+myOrder.addOrder(myPizza);
+var theType = typeof myOrder.pizzaArray[0];
+console.log("type of thing in pizza array " + theType);
+myOrder.printOrder();
+myOrder.addOrder(new Pizza(1,2, ["pepperoni", "ham"]));
+myOrder.printOrder();
