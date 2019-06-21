b80d63d56f4a0427d65cd5cec1a0b86826da2a4c
rochellev <rochellevroberts@utexas.edu>
2019-06-21 12:33:25 -0700

    basic form created

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,42 @@
-function Pizza(size, sauce, toppings){
+function Pizza(size, sauce, toppings) {
     this.size = size,
-    this.sauce = sauce,
-    this.toppingArray = Array(toppings)
+        this.sauce = sauce,
+        this.toppingArray = Array(toppings)
 }
 
-Pizza.prototype.printPizza = function(){
-    console.log("size = "+ this.size + "\n" + "sauce = "+ this.sauce + "\n" + "toppingArray = "+ this.toppingArray + "\n");
+Pizza.prototype.printPizza = function () {
+    console.log("size = " + this.size + "\n" + "sauce = " + this.sauce + "\n" + "toppingArray = " + this.toppingArray + "\n");
 }
 
 // keep track of the pizzas
-function Order(){
+function Order() {
     this.id = 0;
     this.pizzaArray = []
     this.completeOrder = false;
 }
 
-Order.prototype.printOrder = function(){
-    console.log("id = "+ this.id + "\n" + "pizzaArray = "+ this.pizzaArray + "\n" + "completeOrder = "+ this.completeOrder + "\n");
+Order.prototype.printOrder = function () {
+    console.log("id = " + this.id + "\n" + "pizzaArray = " + this.pizzaArray + "\n" + "completeOrder = " + this.completeOrder + "\n");
 }
 
-Order.prototype.addOrder = function(newPizza){
+Order.prototype.addOrder = function (newPizza) {
     this.pizzaArray.push(newPizza);
     this.assignId;
 }
 
-Order.prototype.assignId = function(){
+Order.prototype.assignId = function () {
     this.id++
     return this.id;
 }
 
 // ## Front-End Logic
 
-$(document).ready(function(){
-    $("#startOrder").click(function(event){
+$(document).ready(function () {
+    $("#startOrder").click(function (event) {
         event.preventDefault();
         $("#startOrder").fadeOut();
-        $("#form").fadeIn();
-       
+        $("#formContainer").fadeIn();
+
     })
 })
+
